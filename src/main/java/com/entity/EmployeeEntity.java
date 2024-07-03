e2582f568fb0ffe8ee2445cc897299a1ebe953d0
Mahesh Kumar Kolla <mahi444k0@gmail.com>
2024-07-03 15:07:02 +0530

    Updated

--- src/main/java/com/entity/EmployeeEntity.java
+++ src/main/java/com/entity/EmployeeEntity.java
@@ -1,5 +1,36 @@
 package com.entity;
 
+import javax.persistence.*;
+
+
+
+
+@Entity
+@Table(name="employee")
 public class EmployeeEntity {
+	@Id
+	int eid;
+	String ename;
+	double esal;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public double getEsal() {
+		return esal;
+	}
+	public void setEsal(double esal) {
+		this.esal = esal;
+	}
+	
+	
 
 }
