f027fed96e8870df7fc6707b975509879cddd952
Mahesh Kumar Kolla <mahi444k0@gmail.com>
2024-07-03 15:38:26 +0530

    Updated

--- src/main/java/com/model/EmployeeModel.java
+++ src/main/java/com/model/EmployeeModel.java
@@ -3,6 +3,8 @@ package com.model;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 
 import com.entity.EmployeeEntity;
 
@@ -12,6 +14,9 @@ public class EmployeeModel implements RemoteEmployee {
 
 	@Override
 	public String insert(EmployeeEntity E) {
+		EntityManagerFactory emf = Persistance.createEntityMangerFactory("ESP");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
 		
 		return "Inserted Sucessfully";
 	}
